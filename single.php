c770013f9f71178c2054547b479225298055ad36
Ramsey <rdarling42@gmail.com>
2015-04-02 15:53:01 -0400

    layout for single posts

--- single.php
+++ single.php
@@ -7,26 +7,19 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="post-wrapper">
+    <section id="posts" class="page-permalink"> 
 
-		<?php while ( have_posts() ) : the_post(); ?>
+        <?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
 
 			<?php the_post_navigation(); ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
 		<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
+        <div id="post-details">
+            <?php get_sidebar(); ?>
+        </div>
+    </section>	
+</div>
 <?php get_footer(); ?>
